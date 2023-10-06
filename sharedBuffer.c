63b593e34d9a0e2dbf18c96c40fc8541db0eda0f
Ting-Xiao Miaw <96478096+Ting-Xiao-Miaw@users.noreply.github.com>
2023-10-07 00:04:04 +0800

    Add files via upload

---
+++ sharedBuffer.c
@@ -0,0 +1,5 @@
+#include "sharedBuffer.h"
+
+unsigned int sharedmemory[10]; // Define the shared buffer
+
+// No need to allocate memory here, as it's a global array
---
+++ sharedBuffer.h
@@ -0,0 +1,6 @@
+#ifndef SHARED_BUFFER_H
+#define SHARED_BUFFER_H
+
+extern unsigned int sharedmemory[10]; // Declare the shared buffer
+
+#endif // SHARED_BUFFER_H
